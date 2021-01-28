9ee69dbdfae09511f2fa8d8c339f335d8c4baf7c
Ajiharan <baskaranajiharan1243@gmail.com>
2021-01-28 23:01:42 +0530

    login form added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppHomeComponent } from './app-home/app-home.component';
+import { AppLoginComponent } from './app-login/app-login.component';
+import { AppPageNotFoundComponent } from './app-page-not-found/app-page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppLoginComponent,
+  },
+  {
+    path: 'home',
+    component: AppHomeComponent,
+  },
+
+  {
+    path: '**',
+    component: AppPageNotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
