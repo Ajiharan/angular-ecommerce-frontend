b95fd3f5b576819ea5a4f30ec1af29d97d28d372
Ajiharan <baskaranajiharan1243@gmail.com>
2021-01-28 23:51:24 +0530

    added login form validation

--- src/app/app-login/app-login.component.ts
+++ src/app/app-login/app-login.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-app-login',
   templateUrl: './app-login.component.html',
   styleUrls: ['./app-login.component.scss'],
 })
 export class AppLoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup({
+    emailId: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.loginForm =
+  }
+
+  submitForm() {
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
+  }
 }
