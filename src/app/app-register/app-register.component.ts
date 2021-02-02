6f84403473436682dbd81772c5357bfcbf8b09ff
Ajiharan <baskaranajiharan1243@gmail.com>
2021-02-02 23:53:05 +0530

    fixed issue

--- src/app/app-register/app-register.component.ts
+++ src/app/app-register/app-register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+} from '@angular/forms';
 @Component({
   selector: 'app-app-register',
   templateUrl: './app-register.component.html',
@@ -10,10 +15,30 @@ export class AppRegisterComponent implements OnInit {
   registerForm: FormGroup = new FormGroup({
     emailId: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required, Validators.min(6)]),
-    conformPassword: new FormControl('', [Validators.required]),
+    conformPassword: new FormControl('', [
+      Validators.required,
+      this.checkPasswordMatch.bind(this),
+    ]),
   });
   constructor() {}
 
-  ngOnInit(): void {}
+  checkPasswordMatch(
+    control: AbstractControl
+  ): { [key: string]: boolean } | null {
+    console.log(
+      'this.registerForm?.controls.password.value',
+      this.registerForm?.controls.password.value
+    );
+    if (this.registerForm?.controls.password.value !== control.value) {
+      return { invalidPassword: true };
+    }
+    return null;
+  }
+
+  ngOnInit(): void {
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.conformPassword.updateValueAndValidity();
+    });
+  }
   submitForm() {}
 }
