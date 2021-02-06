7976ca24429fec81c2d75dabd9afb3116135388e
Ajiharan <baskaranajiharan1243@gmail.com>
2021-02-07 00:44:58 +0530

    fixed issue

--- src/app/mat-loading/mat-loading.component.ts
+++ src/app/mat-loading/mat-loading.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { select, Store } from '@ngrx/store';
+import { IUserState } from '../state/userReducer';
 import { registerUserSelector } from '../state/userSelector';
 @Component({
   selector: 'app-mat-loading',
@@ -11,7 +12,7 @@ export class MatLoadingComponent implements OnInit {
   status: string = '';
   loading: boolean = false;
   constructor(
-    @Inject(MAT_DIALOG_DATA) private data: { status: string; loading: boolean },
+    @Inject(MAT_DIALOG_DATA) private data: { status?: any; loading?: any },
     public dialogRef: MatDialogRef<MatLoadingComponent>,
     private store: Store<any>
   ) {
@@ -20,8 +21,13 @@ export class MatLoadingComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.store.select(registerUserSelector).subscribe((res) => {
-      console.log('res', res);
+    this.store.select(registerUserSelector).subscribe((res: IUserState) => {
+      // console.log('res', res, this.loading);
+      if (!res.loading) {
+        setTimeout(() => {
+          this.dialogRef.close();
+        }, 2000);
+      }
     });
     this.dialogRef.updatePosition({
       top: '5rem',
